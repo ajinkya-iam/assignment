d08fa16c0a8da7e77d1412232a9acfd30dada68c
Ajinkya Shinde <ajinkyashinde.me@gmail.com>
2024-12-05 01:03:47 +0530

    code update

--- src/page/CompanyProfile.jsx
+++ src/page/CompanyProfile.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa6";
+import { MdMusicNote } from "react-icons/md";
 
 const CompanyProfile = () => {
+
+    const members = [
+        { role: "CEO #1", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "CEO #3", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "CTO #2", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "CMO #4", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "CEO ", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "CEO ", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "HR", name: "Captain Hook", image: require("../images/profile.png") },
+        { role: "Support", name: "Captain Hook", image: require("../images/profile.png") },
+    ];
+
     return (
         <div>
             <h1 className="text-2xl font-bold">Company Profile</h1>
@@ -21,8 +40,8 @@ const CompanyProfile = () => {
                 <h1 className="text-xl font-medium">Company Informations</h1>
                 <div className="flex gap-4">
                     <div className="flex flex-col gap-4 w-[60%] mt-4 ">
-                        <div className="bg-gray-100 rounded-2xl p-3">
-                            <div className="grid grid-cols-3 gap-4">
+                        <div className="content-center bg-gray-100 rounded-2xl p-3">
+                            <div className="grid grid-cols-3 gap-4 items-center">
                                 {/* First Row */}
                                 <div>
                                     <p className="font-semibold text-sm text-black">Company Name</p>
@@ -37,16 +56,16 @@ const CompanyProfile = () => {
                                     <p className="font-semibold text-sm text-black">Company Size</p>
                                     <p className="text-sm">2 - 10 employees</p>
                                 </div>
-                               <div className="col-span-2">
+                                <div className="col-span-2">
                                     <p className="font-semibold text-sm text-black">Company Description</p>
                                     <p className="text-sm">Peter plan is leading indian multinational provider of outsourcing,offshore software development , web & mobile app developement
-                                    SEO and internet marketing services </p>
+                                        SEO and internet marketing services </p>
                                 </div>
                                 <div>
                                     <p className="font-semibold text-sm text-black">Company Revenue</p>
                                     <p className="text-sm">$100k - $2M</p>
                                 </div>
-                             
+
                                 {/* third Row */}
                                 <div>
                                     <p className="font-semibold text-sm text-black">eMail #1</p>
@@ -89,41 +108,44 @@ const CompanyProfile = () => {
                         </div>
 
                         <div className="bg-gray-100 rounded-2xl p-3">
-
+                            <div className="flex justify-evenly">
+                                <FaFacebook size={28} />
+                                <FaXTwitter size={28} />
+                                <FaInstagram size={28} />
+                                <FaLinkedin size={28} />
+                                <FaGoogle size={28} />
+                                <FaYoutube size={28} />
+                                <MdMusicNote size={28} />
+                            </div>
                         </div>
                     </div>
 
                     <div className="flex flex-col w-[40%] gap-4 mt-4">
                         <div className="bg-gray-100 rounded-2xl p-3">
                             <div className="grid grid-cols-2 gap-4">
-                                {/* First Row */}
-                                <div>
-                                    <p className="font-medium">Company Name</p>
-                                    <p>Peter Pan</p>
-                                </div>
-                                <div>
-                                    <p className="font-medium">Founded</p>
-                                    <p>2017</p>
-                                </div>
-                                {/* Second Row */}
-                                <div>
-                                    <p className="font-medium">Company Size</p>
-                                    <p>2 - 10 employees</p>
-                                </div>
-                                <div>
-                                    <p className="font-medium">Company Revenue</p>
-                                    <p>$100k - $2M</p>
-                                </div>
+                                {members.map(member => (
+                                    <div className="flex">
+                                        <img className="w-12 h-12 rounded-full" src={member.image} />
+                                        <div className="flex flex-col">
+                                            <p className="font-semibold text-sm text-black">{member.role}</p>
+                                            <p className="text-sm">{member.name}</p>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
                         </div>
 
-                        <div className="bg-gray-100 rounded-2xl p-3">
+                        <div className=" bg-gray-100  rounded-2xl p-3">
                             <p className="font-semibold text-sm text-black">Similar Companies</p>
+                            <p className="text-sm"><span className="font-semibold text-sm text-black">WESPEAKCODE</span>| Software Developement Agency</p>
+                            <p className="text-sm"><span className="font-semibold text-sm text-black">LUCENT INNOVATION</span>| Software Developement </p>
+                            <p className="text-sm"><span className="font-semibold text-sm text-black">Microsoft</span>| Software Company</p>
+                            <p className="text-sm"><span className="font-semibold text-sm text-black">Google</span>| Software Company </p>
                         </div>
                     </div>
                 </div>
             </div>
- 
+
         </div>
     );
 };
